c82d198c8389912108e5e9553c4cec7f771b937a
Nayless <32551802+Nayless@users.noreply.github.com>
2018-01-28 22:11:00 +0300

    Update Code.py

--- Code.py
+++ Code.py
@@ -3,7 +3,9 @@ sOut=open("result.txt","w")
 c=0
 summ=0
 for i in sIn:
-    summ+=int(sIn.readline())
+    p=sIn.readline()
+    p=p[0]
+    summ+=int(p)
     c+=1
 sOut.write(str(summ//c))
 sOut.close()
